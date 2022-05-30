48eafcad8acb8b81b08c2899177281b2987245a1
Smileycorp <callum.j.longbottom@gmail.com>
2022-05-30 01:11:36 +0100

    fix crash when spawning player zombies on servers

--- src/main/java/net/smileycorp/hordes/common/entities/EntityZombiePlayer.java
+++ src/main/java/net/smileycorp/hordes/common/entities/EntityZombiePlayer.java
@@ -26,55 +26,55 @@ import com.mojang.authlib.GameProfile;
 
 
 public class EntityZombiePlayer extends EntityZombie {
-	
+
 	protected static final DataParameter<Optional<UUID>> PLAYER_UUID = EntityDataManager.createKey(EntityZombiePlayer.class, DataSerializers.OPTIONAL_UNIQUE_ID);
-	
+
 	protected NonNullList<ItemStack> playerItems = NonNullList.<ItemStack>create();
 	protected UUID uuid;
-	
+
 	public EntityZombiePlayer(World world) {
 		super(world);
 	}
-	
+
 	public EntityZombiePlayer(EntityPlayer player) {
 		this(player.world);
 		setPlayer(player);
 	}
-	
+
 
 	@Override
 	protected void entityInit() {
-        super.entityInit();
-        dataManager.register(PLAYER_UUID, Optional.of(UUID.fromString("1512ce82-00e5-441a-9774-f46d9b7badfb")));
-    }
-	
+		super.entityInit();
+		dataManager.register(PLAYER_UUID, Optional.of(UUID.fromString("1512ce82-00e5-441a-9774-f46d9b7badfb")));
+	}
+
 	public void setPlayer(EntityPlayer player) {
 		for (EntityEquipmentSlot slot : EntityEquipmentSlot.values()) {
-			ItemStack stack = slot.getSlotType() == EntityEquipmentSlot.Type.ARMOR ? player.inventory.armorItemInSlot(slot.getIndex()) :
+			ItemStack stack = slot.getSlotType() == EntityEquipmentSlot.Type.ARMOR ? player.inventory.armorInventory.get(slot.getIndex()) :
 				slot == EntityEquipmentSlot.MAINHAND ? player.getHeldItemMainhand() : player.getHeldItemOffhand();
-			setItemStackToSlot(slot, stack);
+				setItemStackToSlot(slot, stack);
 		}
 		setPlayer(player.getGameProfile());
 	}
-	
+
 	public void setPlayer(String username) {
 		setPlayer(FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerProfileCache().getGameProfileForUsername(username));
 	}
-	
+
 	public void setPlayer(UUID uuid) {
 		setPlayer(FMLCommonHandler.instance().getMinecraftServerInstance().getPlayerProfileCache().getProfileByUUID(uuid));
 	}
-	
+
 	public void setPlayer(GameProfile profile) {
-		this.uuid=profile.getId();
-		this.setCustomNameTag(profile.getName());
+		uuid=profile.getId();
+		setCustomNameTag(profile.getName());
 		dataManager.set(PLAYER_UUID, Optional.of(uuid));
 	}
 
 	public UUID getPlayerUUID() {
 		return dataManager.get(PLAYER_UUID).get();
 	}
-	
+
 	public void setInventory(List<EntityItem> list) {
 		playerItems.clear();
 		for (EntityItem item : list) {
@@ -83,47 +83,47 @@ public class EntityZombiePlayer extends EntityZombie {
 			if (stack != null) playerItems.add(stack.copy());
 		}
 	}
-	
+
 	@Override
 	protected void dropEquipment(boolean recentlyHit, int looting) {
 		for (ItemStack stack : playerItems) {
-            if (!stack.isEmpty() && ! EnchantmentHelper.hasVanishingCurse(stack)) {
-                entityDropItem(stack, 0f);
-            }
-        }
-    }
-	
+			if (!stack.isEmpty() && ! EnchantmentHelper.hasVanishingCurse(stack)) {
+				entityDropItem(stack, 0f);
+			}
+		}
+	}
+
 	@Override
 	public boolean shouldBurnInDay() {
 		return false;
 	}
-	
+
 	@Override
 	public void writeEntityToNBT(NBTTagCompound compound) {
-        super.writeEntityToNBT(compound);
-        if (uuid != null) {
-        	compound.setString("player", uuid.toString());
-        }
-        ItemStackHelper.saveAllItems(compound, playerItems);
-    }
-	
-    @Override
+		super.writeEntityToNBT(compound);
+		if (uuid != null) {
+			compound.setString("player", uuid.toString());
+		}
+		ItemStackHelper.saveAllItems(compound, playerItems);
+	}
+
+	@Override
 	public void readEntityFromNBT(NBTTagCompound compound) {
-       super.readEntityFromNBT(compound);
-       if (compound.hasKey("player")) {
-    	   uuid = UUID.fromString(compound.getString("player"));
-       }
-       NonNullList<ItemStack> read = NonNullList.<ItemStack>withSize(compound.getTagList("Items", 10).tagCount(), ItemStack.EMPTY);
-       ItemStackHelper.loadAllItems(compound, read);
-       playerItems = read;
-    }
-	
+		super.readEntityFromNBT(compound);
+		if (compound.hasKey("player")) {
+			uuid = UUID.fromString(compound.getString("player"));
+		}
+		NonNullList<ItemStack> read = NonNullList.<ItemStack>withSize(compound.getTagList("Items", 10).tagCount(), ItemStack.EMPTY);
+		ItemStackHelper.loadAllItems(compound, read);
+		playerItems = read;
+	}
+
 	@Override
 	public ITextComponent getDisplayName() {
-        TextComponentTranslation textcomponentstring = new TextComponentTranslation(ScorePlayerTeam.formatPlayerName(getTeam(), "entity.hordes.ZombiePlayer.chat"), ScorePlayerTeam.formatPlayerName(getTeam(), getName()));
-        textcomponentstring.getStyle().setHoverEvent(this.getHoverEvent());
-        textcomponentstring.getStyle().setInsertion(this.getCachedUniqueIdString());
-        return textcomponentstring;
-    }
-	
+		TextComponentTranslation textcomponentstring = new TextComponentTranslation(ScorePlayerTeam.formatPlayerName(getTeam(), "entity.hordes.ZombiePlayer.chat"), ScorePlayerTeam.formatPlayerName(getTeam(), getName()));
+		textcomponentstring.getStyle().setHoverEvent(getHoverEvent());
+		textcomponentstring.getStyle().setInsertion(getCachedUniqueIdString());
+		return textcomponentstring;
+	}
+
 }
