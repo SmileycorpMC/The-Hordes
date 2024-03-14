46c9fe330cfde5b0e53a59a71731320fb070bdbb
Smileycorp <callum.j.longbottom@gmail.com>
2024-03-14 17:33:30 +0000

    make biome condition be able to use tags and lists

--- src/main/java/net/smileycorp/hordes/common/data/conditions/BiomeCondition.java
+++ src/main/java/net/smileycorp/hordes/common/data/conditions/BiomeCondition.java
@@ -1,32 +1,52 @@
 package net.smileycorp.hordes.common.data.conditions;
 
+import com.google.common.collect.Lists;
 import com.google.gson.JsonElement;
+import com.mojang.datafixers.util.Either;
+import net.minecraft.core.Holder;
+import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.biome.Biome;
 import net.smileycorp.hordes.common.HordesLogger;
 
+import java.util.List;
+
 public class BiomeCondition implements Condition {
-
-	protected ResourceLocation biome;
-
-	public BiomeCondition(ResourceLocation biome ) {
-		this.biome = biome;
+	
+	protected final List<Either<TagKey<Biome>, ResourceLocation>> biomes;
+	
+	public BiomeCondition(List<Either<TagKey<Biome>, ResourceLocation>> biomes) {
+		this.biomes = biomes;
 	}
 
 	@Override
 	public boolean apply(Level level, LivingEntity entity, RandomSource rand) {
-		return level.getBiomeManager().getBiome(entity.blockPosition()).is(biome);
+		Holder<Biome> biome = level.getBiomeManager().getBiome(entity.blockPosition());
+		for (Either<TagKey<Biome>, ResourceLocation> either : biomes) if (either.map(biome::is, biome::is)) return true;
+		return false;
 	}
 
 	public static BiomeCondition deserialize(JsonElement json) {
 		try {
-			return new BiomeCondition(new ResourceLocation(json.getAsString()));
+			if (json.isJsonArray()) {
+				List<Either<TagKey<Biome>, ResourceLocation>> biomes = Lists.newArrayList();
+				for (JsonElement element : json.getAsJsonArray()) biomes.add(either(element.getAsString()));
+				return new BiomeCondition(biomes);
+			}
+			return new BiomeCondition(Lists.newArrayList(either(json.getAsString())));
 		} catch(Exception e) {
 			HordesLogger.logError("Incorrect parameters for condition hordes:biome", e);
 		}
 		return null;
 	}
-
+	
+	private static Either<TagKey<Biome>, ResourceLocation> either(String string) {
+		return string.contains("#") ? Either.left(TagKey.create(Registries.BIOME, new ResourceLocation(string.replace("#", ""))))
+				: Either.right(new ResourceLocation(string));
+	}
+	
 }
