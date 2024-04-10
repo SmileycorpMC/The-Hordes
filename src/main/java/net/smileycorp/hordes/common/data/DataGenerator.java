2873d99a73d65795a241ce5977d894178d36bf58
Smileycorp <callum.j.longbottom@gmail.com>
2024-04-11 00:00:54 +0100

    make data generator dynamically search for files instead of using a pre-programmed list

--- src/main/java/net/smileycorp/hordes/common/data/DataGenerator.java
+++ src/main/java/net/smileycorp/hordes/common/data/DataGenerator.java
@@ -4,16 +4,19 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import net.minecraftforge.fml.loading.FMLLoader;
 import net.minecraftforge.fml.loading.FMLPaths;
+import net.minecraftforge.fml.loading.moddiscovery.ModFile;
 import net.smileycorp.atlas.api.util.DataUtils;
 import net.smileycorp.hordes.common.HordesLogger;
 import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jetbrains.annotations.NotNull;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
+import java.io.*;
+import java.net.URI;
+import java.nio.file.*;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class DataGenerator {
 
@@ -31,47 +34,32 @@ public class DataGenerator {
             }
             FileUtils.deleteDirectory(CONFIG_FOLDER.toFile());
         } catch (Exception e) {}
-        DataUtils.copyFileFromMod("hordes-info.json", "hordes");
-        DataUtils.copyFileFromMod("pack.mcmeta", "hordes");
+        ModFile mod = FMLLoader.getLoadingModList().getModFileById("hordes").getFile();
+        copyFileFromMod(mod.findResource("config_defaults/hordes-info.json"));
+        copyFileFromMod(mod.findResource("config_defaults/pack.mcmeta"));
         return true;
     }
-
+    
     public static void generateAssets() {
-        DataUtils.copyFileFromMod("assets/hordes/lang/en_us.json", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/lang/ru_ru.json", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/sounds.json", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/sounds/horde_spawn.ogg", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/entity/layer/drowned_player_outer_layer.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/entity/layer/husk_player_outer_layer.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/entity/layer/zombie_player_outer_layer.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/entity/zombie_evoker.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/entity/zombie_illusioner.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/entity/zombie_pillager.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/entity/zombie_vindicator.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/entity/zombie_wandering_trader.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/entity/zombie_witch.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/entity/zombified_piglin_brute.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/gui/jei/cure_list.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/mob_effect/immunity.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/mob_effect/infected.png", "hordes");
-        DataUtils.copyFileFromMod("assets/hordes/textures/mob_effect/infected.png.mcmeta", "hordes");
-        HordesLogger.logInfo("Generated asset files.");
+        ModFile mod = FMLLoader.getLoadingModList().getModFileById("hordes").getFile();
+        try {
+            Files.find(mod.findResource("config_defaults/assets"), Integer.MAX_VALUE, (matcher, options) -> options.isRegularFile())
+                    .forEach(DataGenerator::copyFileFromMod);
+            HordesLogger.logInfo("Generated asset files.");
+        } catch (Exception e) {
+            HordesLogger.logInfo("Failed to generate asset files.");
+        }
     }
 
     public static void generateData() {
-        DataUtils.copyFileFromMod("data/hordes/horde_data/immunity_items.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/horde_data/infection_conversions.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/horde_data/scripts/default.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/horde_data/tables/default.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/horde_data/tables/drowned.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/horde_data/tables/illagers.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/horde_data/tables/mixed_mobs.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/horde_data/tables/skeletons.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/tags/entity_types/infection_entities.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/tags/items/infection_cures.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/tags/items/infection_immune_wearables.json", "hordes");
-        DataUtils.copyFileFromMod("data/hordes/tags/worldgen/biome/husk_player_spawn_biomes.json", "hordes");
-        HordesLogger.logInfo("Generated data files.");
+        ModFile mod = FMLLoader.getLoadingModList().getModFileById("hordes").getFile();
+        try {
+            Files.find(mod.findResource("config_defaults/data"), Integer.MAX_VALUE, (matcher, options) -> options.isRegularFile())
+                    .forEach(DataGenerator::copyFileFromMod);
+            HordesLogger.logInfo("Generated data files.");
+        } catch (Exception e) {
+            HordesLogger.logInfo("Failed to generate data files.");
+        }
     }
 
     private static boolean isUpToDate() {
@@ -92,6 +80,20 @@ public class DataGenerator {
         } catch (Exception e) {
             HordesLogger.logError("Failed data version check", e);
             return false;
+        }
+    }
+    
+    private static void copyFiles(Path path) throws IOException {
+    
+    }
+    
+    private static void copyFileFromMod(Path path) {
+        try {
+            FileUtils.copyInputStreamToFile(Files.newInputStream(path),
+                    new File(CONFIG_FOLDER.toFile(), path.toString().replace( "config_defaults/", "")));
+            HordesLogger.logInfo("Copied file " + path);
+        } catch (Exception e) {
+            HordesLogger.logError("Failed to copy file " + path, e);
         }
     }
 
