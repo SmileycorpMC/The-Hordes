f9fd741669b5b03e79cf8b06c2b689208c45a577
Smileycorp <callum.j.longbottom@gmail.com>
2024-10-14 11:40:58 +0100

    fix issues with loc on non english languages

--- src/main/java/net/smileycorp/hordes/common/Constants.java
+++ src/main/java/net/smileycorp/hordes/common/Constants.java
@@ -3,6 +3,8 @@ package net.smileycorp.hordes.common;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.sounds.SoundEvent;
 
+import java.util.Locale;
+
 public class Constants {
 	
 	public static final String MODID = "hordes";
@@ -25,7 +27,7 @@ public class Constants {
 	}
 	
 	public static ResourceLocation loc(String name) {
-		return new ResourceLocation(MODID, name.toLowerCase());
+		return new ResourceLocation(MODID, name.toLowerCase(Locale.US));
 	}
 	
 	public static String locStr(String string) {
