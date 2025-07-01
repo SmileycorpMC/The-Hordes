5b656eb98d97761cc04743b12c3804ac7ced01d5
Smileycorp <callum.j.longbottom@gmail.com>
2025-07-01 21:36:44 +0100

    add zombie player cape render config

--- src/main/java/net/smileycorp/hordes/config/ClientConfigHandler.java
+++ src/main/java/net/smileycorp/hordes/config/ClientConfigHandler.java
@@ -3,7 +3,6 @@ package net.smileycorp.hordes.config;
 import com.google.common.collect.Lists;
 import net.minecraft.network.chat.TextColor;
 import net.minecraftforge.common.ForgeConfigSpec;
-import net.minecraftforge.common.ForgeConfigSpec.ConfigValue;
 import net.smileycorp.hordes.common.HordesLogger;
 
 import java.awt.*;
@@ -12,40 +11,43 @@ import java.util.List;
 
 public class ClientConfigHandler {
 
-	public static final ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 	public static final ForgeConfigSpec config;
 
+	//horde event
+	public static ForgeConfigSpec.ConfigValue<Integer> eventNotifyMode;
+	public static ForgeConfigSpec.ConfigValue<Integer> eventNotifyDuration;
+	public static ForgeConfigSpec.ConfigValue<Boolean> hordeSpawnSound;
+	private static ForgeConfigSpec.ConfigValue<List<? extends Integer>> configHordeMessageColour;
+	public static ForgeConfigSpec.ConfigValue<Boolean> hordeEventTintsSky;
+	public static ForgeConfigSpec.ConfigValue<List<? extends Integer>> configHordeEventSkyColour;
+	public static ForgeConfigSpec.ConfigValue<List<? extends Integer>> configHordeEventMoonColour;
+
 	private static TextColor hordeMessageColour = null;
-	private static Color zombiePlayerColour = null;
-	private static Color drownedPlayerColour = null;
-	private static Color huskPlayerColour = null;
 	private static Color hordeEventSkyColour = null;
 	private static Color hordeEventMoonColour = null;
 
-	//horde event
-	public static ConfigValue<Integer> eventNotifyMode;
-	public static ConfigValue<Integer> eventNotifyDuration;
-	public static ConfigValue<Boolean> hordeSpawnSound;
-	private static ConfigValue<List<? extends Integer>> configHordeMessageColour;
-	public static ConfigValue<Boolean> hordeEventTintsSky;
-	public static ConfigValue<List<? extends Integer>> configHordeEventSkyColour;
-	public static ConfigValue<List<? extends Integer>> configHordeEventMoonColour;
-
-
 	//infection
-	public static ConfigValue<Boolean> playerInfectionVisuals;
-	public static ConfigValue<Boolean> playerInfectSound;
-	public static ConfigValue<Boolean> infectionProtectSound;
-	public static ConfigValue<Boolean> cureTooltip;
-	public static ConfigValue<Boolean> immunityTooltip;
-	public static ConfigValue<Boolean> wearableProtectionTooltip;
-	private static ConfigValue<List<? extends Integer>> configZombiePlayerColour;
-	private static ConfigValue<List<? extends Integer>> configDrownedPlayerColour;
-	private static ConfigValue<List<? extends Integer>> configHuskPlayerColour;
+	public static ForgeConfigSpec.ConfigValue<Boolean> playerInfectionVisuals;
+	public static ForgeConfigSpec.ConfigValue<Boolean> playerInfectSound;
+	public static ForgeConfigSpec.ConfigValue<Boolean> infectionProtectSound;
+	public static ForgeConfigSpec.ConfigValue<Boolean> cureTooltip;
+	public static ForgeConfigSpec.ConfigValue<Boolean> immunityTooltip;
+	public static ForgeConfigSpec.ConfigValue<Boolean> wearableProtectionTooltip;
+
+	//zombie players
+	private static ForgeConfigSpec.ConfigValue<List<? extends Integer>> configZombiePlayerColour;
+	private static ForgeConfigSpec.ConfigValue<List<? extends Integer>> configDrownedPlayerColour;
+	private static ForgeConfigSpec.ConfigValue<List<? extends Integer>> configHuskPlayerColour;
+	public static ForgeConfigSpec.ConfigValue<Boolean> zombiePlayerCapes;
+
+	private static Color zombiePlayerColour = null;
+	private static Color drownedPlayerColour = null;
+	private static Color huskPlayerColour = null;
 
 	//load config properties
 	static {
 		HordesLogger.logInfo("Trying to load client config");
+		ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
 		//horde event
 		builder.push("Horde Event");
 		eventNotifyMode = builder.comment("How do players get notified of a horde event. 0: Off, 1: Chat, 2:Action Bar, 3:Title").define("eventNotifyMode", 2);
@@ -68,12 +70,16 @@ public class ClientConfigHandler {
 		cureTooltip = builder.comment("Show a tooltip on items that can cure infection?").define("cureTooltip", true);
 		immunityTooltip = builder.comment("Show a tooltip on items that give infection immunity?").define("immunityTooltip", true);
 		wearableProtectionTooltip = builder.comment("Show a tooltip on wearable items that grant some protection against infection?").define("wearableProtectionTooltip", true);
+		builder.pop();
+
+		builder.push("Zombie Players");
 		configZombiePlayerColour = builder.comment("Colour tint for zombie players.")
 				.defineList("zombiePlayerColour", Lists.newArrayList(121, 156, 101), x -> (int)x >= 0 && (int)x < 256);
 		configDrownedPlayerColour = builder.comment("Colour tint for drowned players.")
 				.defineList("drownedPlayerColour", Lists.newArrayList(144, 255, 255), x -> (int)x >= 0 && (int)x < 256);
 		configHuskPlayerColour = builder.comment("Colour tint for husk players.")
 				.defineList("huskPlayerColour", Lists.newArrayList(193, 168, 5), x -> (int)x >= 0 && (int)x < 256);
+		zombiePlayerCapes = builder.comment("Render zombie players with capes if the player that got infected has one equipped?").define("zombiePlayerCapes", true);
 		builder.pop();
 		config = builder.build();
 	}
