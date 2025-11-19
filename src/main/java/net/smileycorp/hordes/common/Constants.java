0d508e6ba29260196ec7d8d1f7acc1001526e2c7
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2025-11-19 01:58:06 +0000

    remove shadow from build and update atlas lib version

--- src/main/java/net/smileycorp/hordes/common/Constants.java
+++ src/main/java/net/smileycorp/hordes/common/Constants.java
@@ -10,7 +10,7 @@ public class Constants {
 	public static final String MODID = "hordes";
 	public static final String NAME = "The Hordes";
 	public static final String VERSION = "1.5.3b";
-	public static final String DEPENDENCIES = "required-after:atlaslib@1.1.5;after:baubles;after:galacticraft;before:deathchest;before:corpse;before:jei";
+	public static final String DEPENDENCIES = "required-after:atlaslib@[1.1.9,);after:baubles;after:galacticraft;before:deathchest;before:corpse;before:jei";
 	private static final String PACKAGE = "net.smileycorp.hordes.";
 	public static final String CLIENT_PROXY = PACKAGE + "client.ClientProxy";
 	public static final String SERVER_PROXY = PACKAGE + "common.CommonProxy";
