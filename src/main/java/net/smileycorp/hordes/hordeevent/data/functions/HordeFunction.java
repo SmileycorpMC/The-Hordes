dc9126eee2380401a1e53618934e8c06612f2d31
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-05-15 21:41:07 +0100

    add function deserializer interface

--- src/main/java/net/smileycorp/hordes/hordeevent/data/functions/HordeFunction.java
+++ src/main/java/net/smileycorp/hordes/hordeevent/data/functions/HordeFunction.java
@@ -1,10 +1,17 @@
 package net.smileycorp.hordes.hordeevent.data.functions;
 
+import com.google.gson.JsonElement;
 import net.smileycorp.hordes.common.event.HordePlayerEvent;
 import net.smileycorp.hordes.hordeevent.data.HordeContext;
 
 public interface HordeFunction<T extends HordePlayerEvent> {
 
 	void apply(HordeContext<T> ctx);
+
+	interface Deserializer<T extends HordePlayerEvent> {
+
+		HordeFunction<T> apply(JsonElement element);
+
+	}
 
 }
