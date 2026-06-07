8eb26919e1153bff07aeea9feb763619d0d5c34d
Smileycorp <11587363+SmileycorpMC@users.noreply.github.com>
2026-06-07 16:22:16 +0100

    fix atlas lib dependency version

--- src/main/java/net/smileycorp/hordes/common/Constants.java
+++ src/main/java/net/smileycorp/hordes/common/Constants.java
@@ -11,7 +11,7 @@ public class Constants {
 	public static final String NAME = "The Hordes";
 
 	public static final String VERSION = "1.6.3";
-	public static final String DEPENDENCIES = "required-after:atlaslib@[1.1.10,);after:baubles;after:galacticraft;before:deathchest;before:corpse;before:jei";
+	public static final String DEPENDENCIES = "required-after:atlaslib@[1.1.11,);after:baubles;after:galacticraft;before:deathchest;before:corpse;before:jei";
 
 	private static final String PACKAGE = "net.smileycorp.hordes.";
 	public static final String CLIENT_PROXY = PACKAGE + "client.ClientProxy";
